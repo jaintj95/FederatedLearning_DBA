6dd949f321fc3e762d10929ee2010a7103bab2f8
Tejas Jain <jaintj95@users.noreply.github.com>
2020-06-01 21:06:34 +0530

    fix module for windows

--- utils/tinyimagenet_reformat.py
+++ utils/tinyimagenet_reformat.py
@@ -5,6 +5,7 @@ import os
 from shutil import move
 from os.path import join
 from os import listdir, rmdir
+import platform
 
 target_folder = '../data/tiny-imagenet-200/val/'
 
@@ -14,16 +15,22 @@ with open(target_folder + 'val_annotations.txt', 'r') as f:
         split_line = line.split('\t')
         val_dict[split_line[0]] = split_line[1]
 
-paths = glob.glob(target_folder + 'images/*')
-paths[0].split('/')[-1]
+paths = glob.glob(target_folder + 'images/*.JPEG')
+
 for path in paths:
-    file = path.split('/')[-1]
+    if platform.system() == 'Windows':
+        file = path.split('\\')[-1]
+    else:
+        file = path.split('/')[-1]
     folder = val_dict[file]
     if not os.path.exists(target_folder + str(folder)):
         os.mkdir(target_folder + str(folder))
 
 for path in paths:
-    file = path.split('/')[-1]
+    if platform.system() == 'Windows':
+        file = path.split('\\')[-1]
+    else:
+        file = path.split('/')[-1]
     folder = val_dict[file]
     dest = target_folder + str(folder) + '/' + str(file)
     move(path, dest)
